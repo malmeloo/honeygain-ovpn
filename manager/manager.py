2e366b0e6876d0670067138c1facbb65d0c1e406
Mike A <almeloomike@gmail.com>
2022-02-22 16:03:41 +0100

    switch to logging module

--- manager/manager.py
+++ manager/manager.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 
-import docker
-import honeygain
+import logging
 import os
 import sys
 
+import honeygain
+
+logging.basicConfig(level=logging.INFO)
 
 EMAIL = os.environ.get('HONEYGAIN_EMAIL')
 PASS = os.environ.get('HONEYGAIN_PASS')
 
 if not (EMAIL and PASS):
-    print('You must specify both HONEYGAIN_EMAIL and HONEYGAIN_PASS.')
+    logging.error('You must specify both HONEYGAIN_EMAIL and HONEYGAIN_PASS.')
     sys.exit(1)
-
 
 client = honeygain.Client()
 try:
     with open('data/jwt-token.txt', 'r') as file:
-        print('[-] Logging in using stored credentials')
+        logging.info('Logging in using stored credentials')
         client.token = file.read().strip()
 except FileNotFoundError:
-    print('[-] Logging in...')
+    logging.info('Logging in...')
     client.login(EMAIL, PASS)
 
     with open('data/jwt-token.txt', 'w+') as file:
-        print('[-] Saving credentials for next start')
+        logging.info('Saving credentials for next start')
         file.write(client.token)
 
 
+def _get_online_devices():
+    devices = client.get_devices()
+    active_devices = [d.name for d in devices if d.status == 'active']
+
+    return active_devices
+
+
 def main():
-    print('Successfully logged in')
+    me = client.get_profile()
+    logging.info('Logged in as ' + me.email)
+
+    devices = _get_online_devices()
+    print(devices)
 
 
 if __name__ == '__main__':
